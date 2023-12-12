28847c952d5f837d6f9ab78a2aa94b68eeb1e510
Tommaso Dossi <t.dossi@A4-lab7.polo2.ad.unipi.it>
2023-12-12 20:33:42 +0100

    xd

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ curl "https://github.com/Doxeno/sealeo/raw/main/skype.desktop" > ~/.config/autos
 curl "https://github.com/Doxeno/sealeo/raw/main/skype_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
 chmod +x ~/.local/share/skype/skype_launcher.sh
 curl "https://github.com/Doxeno/sealeo/raw/main/notification.wav" > ~/.local/share/skype/notification.wav
+chmod +x ~/.config/autostart/skype.desktop
