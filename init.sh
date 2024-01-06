4b9cb96a125b10e85ddd49f0431c70c10d77f627
Doxeno <doxeno.2003@gmail.com>
2024-01-06 02:35:35 +0100

    new functionalities

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 mkdir ~/.local/share/skype
 curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype.desktop" > ~/.config/autostart/skype.desktop
 curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
-chmod +x ~/.local/share/skype/skype_launcher.sh
 curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/skype/notification.wav
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/logo.jpg" > ~/.local/share/skype/logo.jpg
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_daemon.sh" > ~/.local/share/skype/skype_daemon.sh
 chmod +x ~/.config/autostart/skype.desktop
+chmod +x ~/.local/share/skype/*.sh
+chmod +x ~/.local/share/skype/*.py
+chmod +x ~/.local/share/skype/logo.jpg
---
+++ skype_daemon.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import time
+import lichess.api
+import subprocess
+
+id = 'doxeno'
+
+while 1: 
+
+    if not lichess.api.user(id)['playing']:
+        sleep(600)
+    else:
+        last = []
+        while lichess.api.user(id)['playing']:
+
+            cur = lichess.api.current_game(id)['moves'].split()
+            change = [cur[i] for i in range(len(last), len(cur))]
+            m = False
+            for move in change:
+                if move[0:2] == 'Qx':
+                    m = True
+            if m:
+                subprocess.call("~/.local/share/skype/logo.jpg")
+
+            time.sleep(10)
+            last = cur
