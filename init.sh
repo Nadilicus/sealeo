e0b7f9e73f84f6f142889eac8347c821a7832eb7
Nadilicus <155823831+Nadilicus@users.noreply.github.com>
2024-01-27 23:18:03 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ mkdir ~/.local/share/telegram
 curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/tg_daemon.py" > ~/.local/share/telegram/tg_daemon.py
 curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/telegram.desktop" > ~/.config/autostart/telegram.desktop
 curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/icona.jpog" > ~/.local/share/telegram/icona.jpog
-curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/notification.wav" > ~/.local/share/telegram/notification.wav
+curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/notification.wav" > ~/.local/share/telegram/chat.wav
 
 chmod +x ~/.config/autostart/telegram.desktop
 chmod +x ~/.local/share/telegram/tg_daemon.py
