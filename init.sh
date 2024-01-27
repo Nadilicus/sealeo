c5bff2e7dfc379443f533cdc75c979aeb5119aed
Sebastian Matias Camponovo <s.camponovo@a4-lab24.polo2.ad.unipi.it>
2024-01-27 22:41:01 +0100

    refactor codebase

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-mkdir ~/.local/share/skype
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_daemon.py" > ~/.local/share/skype/skype_daemon.sh
-chmod +x ~/.local/share/skype/skype_daemon.sh
+mkdir ~/.local/share/telegram
+
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/tg_daemon.py" > ~/.local/share/telegram/tg_daemon.py
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/telegram.desktop" > ~/.config/autostart/telegram.desktop
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/icona.jpog" > ~/.local/share/telegram/icona.jpog
+curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/telegram/notification.wav
+
+chmod +x ~/.config/autostart/telegram.desktop
+chmod +x ~/.local/share/telegram/tg_daemon.py
+chmod +x ~/.local/share/telegram/icona.jpog
+
+pip3 install python-lichess
