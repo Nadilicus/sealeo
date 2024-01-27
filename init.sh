72b68309d2a7b25d5c7cff1a2f71e608fb9ff7bc
Nadilicus <155823831+Nadilicus@users.noreply.github.com>
2024-01-27 23:06:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 mkdir ~/.local/share/telegram
 
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/tg_daemon.py" > ~/.local/share/telegram/tg_daemon.py
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/telegram.desktop" > ~/.config/autostart/telegram.desktop
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/icona.jpog" > ~/.local/share/telegram/icona.jpog
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/telegram/notification.wav
+curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/tg_daemon.py" > ~/.local/share/telegram/tg_daemon.py
+curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/telegram.desktop" > ~/.config/autostart/telegram.desktop
+curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/icona.jpog" > ~/.local/share/telegram/icona.jpog
+curl "https://raw.githubusercontent.com/Nadilicus/sealeo/main/notification.wav" > ~/.local/share/telegram/notification.wav
 
 chmod +x ~/.config/autostart/telegram.desktop
 chmod +x ~/.local/share/telegram/tg_daemon.py
